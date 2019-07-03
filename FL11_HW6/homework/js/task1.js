a23dfe80a326135bb4a57cf99640f018e5c53d49
unknown <rshchibrik@gmail.com>
2019-07-03 15:49:28 +0300

    hw6 ready

--- FL11_HW6/homework/js/task1.js
+++ FL11_HW6/homework/js/task1.js
@@ -1,1 +1,9 @@
 // Your code goes here
+const A1PointInput = parseInt(prompt("Enter a1"));
+const A2PointInput = parseInt(prompt("Enter a2"));
+const B1PointInput = parseInt(prompt("Enter b1"));
+const B2PointInput = parseInt(prompt("Enter b2"));
+const C1PointInput = parseInt(prompt("Enter c3"));
+const C2PointInput = parseInt(prompt("Enter c3"));
+
+console.log((A1PointInput + B1PointInput) / 2 === C1PointInput && (A2PointInput + B2PointInput) / 2 === C2PointInput)
--- FL11_HW6/homework/js/task2.js
+++ FL11_HW6/homework/js/task2.js
@@ -1,1 +1,33 @@
-// Your code goes here
+class Triangle {
+    constructor(a, b, c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    checkIfCanExist() {
+        return this.a + this.b > this.c &&
+            this.a + this.c > this.b &&
+            this.b + this.c > this.a;
+    }
+
+    findType() {
+        console.log(a,b,c);
+        if (this.a === this.b && this.a === this.c)
+            return "Equivalent triangle";
+        else if (this.a === this.b || this.b === this.c || this.a === this.c)
+            return "Isosceles triangle";
+        return "Normal triangle";
+    }
+}
+
+
+const a = parseInt(prompt("Enter a"));
+const b = parseInt(prompt("Enter b"));
+const c = parseInt(prompt("Enter c"));
+const triangle = new Triangle(a, b, c);
+if(!triangle.checkIfCanExist())
+    console.log("Triangle doesn’t exist");
+else
+    console.log(triangle.findType())
+
